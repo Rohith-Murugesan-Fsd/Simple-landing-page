e4aee1867798fc8738d3608c46dbc764904de066
Rohith-Murugesan-Fsd <rohithmurugesan05@gmail.com>
2024-07-06 20:45:07 +0530

    simple Landing page using Html,Css,Js.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener to the Learn More button
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('You clicked the Learn More button!');
+});
+
+// Add event listener to the Sign Up button
+document.querySelector('.cta .btn').addEventListener('click', function() {
+    alert('You clicked the Sign Up button!');
+});
